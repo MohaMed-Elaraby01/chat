438a34cec77da969a4d6596a60ecf4f6a523e8cf
Mohamed Elaraby <126242799+MohaMed-Elaraby01@users.noreply.github.com>
2026-02-24 00:23:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getMessaging, getToken } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js";
 
 // Firebase
 const firebaseConfig = {
@@ -14,6 +16,26 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+const messaging = getMessaging(app);
+
+async function requestPermission() {
+  const permission = await Notification.requestPermission();
+
+  if (permission === "granted") {
+    const token = await getToken(messaging, {
+      vapidKey: "BDHSxQI5ZGB6c5dLprdcmMdnkMqOubjnm7X_vRA_HpDffre8AgxCRFypQPHnqBy6dfCfBj70d35h5IYGoOCajdE"
+    });
+
+    console.log("Token:", token);
+
+    await addDoc(collection(db, "tokens"), {
+      token: token
+    });
+  }
+}
+
+requestPermission();
 
 const messagesRef = collection(db, "messages");
 const q = query(messagesRef, orderBy("time"));
