7cdf1acb6d79e31379b00e7d7bc306c59b8bbeae
Mohamed Elaraby <126242799+MohaMed-Elaraby01@users.noreply.github.com>
2026-03-30 18:52:00 +0200

    Update Firebase configuration in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,10 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAmElPVCV5bIoUste1aunreoWswNak_hh8",
-  authDomain: "chat-e3c2a.firebaseapp.com",
-  projectId: "chat-e3c2a",
-  messagingSenderId: "732872928958",
-  appId: "1:732872928958:web:11005c036280b63caff8ef"
+  apiKey: "AIzaSyC5FCcxMN3ABX6bsPVvSIyAhOg8KuFvCG8",
+  authDomain: "massage-7e249.firebaseapp.com",
+  projectId: "massage-7e249",
+  storageBucket: "massage-7e249.firebasestorage.app",
+  messagingSenderId: "73041371642",
+  appId: "1:73041371642:web:be66d97827989b8abb4443",
 });
-
-const messaging = firebase.messaging();
